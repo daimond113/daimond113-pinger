b7904998dcd122781ab1fc24bc60431b342aac52
daimond113 <72147841+daimond113@users.noreply.github.com>
2021-01-18 21:45:34 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const axios = require('axios')
 const app = express()
 
 setInterval(() => {
-  const items = process.env.URLs.split(',')
+  const items = process.env.URLS.split(',')
   for (const file of items) {
-    axios.head(items)
+    axios.head(file).catch((e) => {console.log(e)})
   }
 }, 1800000)
 
