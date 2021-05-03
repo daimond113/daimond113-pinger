d1f3b18ec239ca8c76b81b426133f7987b5e4c2a
daimond113 <72147841+daimond113@users.noreply.github.com>
2021-05-04 00:42:06 +0200

    typescript

---
+++ build/index.d.ts
@@ -0,0 +1,3 @@
+declare const express: any;
+declare const axios: any;
+declare const app: any;
---
+++ build/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const axios = require('axios');
+const app = express();
+setInterval(() => {
+    const items = process.env.URLS.split(',');
+    for (const file of items) {
+        axios.head(file).catch((e) => {
+            console.log(e);
+        });
+    }
+}, 1800000);
+app.get('/', (req, res) => {
+    res.status(301).redirect('https://daimond113.com');
+});
+app.listen(3000);
